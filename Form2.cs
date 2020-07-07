4cf0b5db1f449f1a40cbe971eb7bbf42650fafc0
vanashrip <67672432+vanashrip@users.noreply.github.com>
2020-07-07 15:30:33 +0530

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignments
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class StudentModel
+
+        { }
+
+            public string Full_Name { get; set; }
+
+            public int ClassRoom { get; set; }
+
+            public string Teacher_Name { get; set; }
+
+            public string Principle_Name { get; set; }
+
+            public string School_Name { get; set; }
+
+            public string Documents { get; set; }
+ 
+
+        }
+    }
+}
